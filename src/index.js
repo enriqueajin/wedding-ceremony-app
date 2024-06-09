9faf6d90f9b984d9a5f56726b1d38a7f0b636f4f
Enrique Ajin <enriquesaon@gmail.com>
2024-06-09 19:11:35 +0200

    Fix typo in font import.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './fonts/josephsophia.ttf'
 import './fonts/nexaheavy.ttf'
 import './fonts/nexaextralight.ttf'
 import './fonts/antaradistance.ttf'
-import './fonts/miltononebold.tf'
+import './fonts/miltononebold.tff'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
